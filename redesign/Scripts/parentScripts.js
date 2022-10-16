33ab0ee4b6b5dc39c5c92249f324f5f52cd551b7
KP <42572567+kpatel122@users.noreply.github.com>
2022-10-16 12:51:53 +0100

    initial responsive redsign

---
+++ redesign/Scripts/parentScripts.js
@@ -0,0 +1,7 @@
+
+var toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
